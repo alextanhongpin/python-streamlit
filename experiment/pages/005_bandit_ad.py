b0cb479686d34d81db408c62c088272b42fcc858
alextanhongpin <alex.tan@bandlab.com>
2025-03-04 12:26:59 +0800

    chore: update bandit

--- experiment/pages/005_bandit_ad.py
+++ experiment/pages/005_bandit_ad.py
@@ -3,22 +3,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 from lib.bandit import Bandit
 
-st.title("Pick a fruit")
 
+fruits = ["Apple", "Banana", "Orange"]
+images = [
+    "assets/images/apple.jpg",
+    "assets/images/banana.jpg",
+    "assets/images/orange.webp",
+]
 
-n_arms = 3
-if 'bandit' not in st.session_state:
+n_arms = len(fruits)
+if "bandit" not in st.session_state:
     st.session_state.bandit = Bandit(n_arms)
-fruits = ['Apple', 'Banana', 'Orange']
-
-cols = st.columns(3)
-for i, col in enumerate(cols):
-    if col.button(fruits[i], use_container_width=True):
-        st.session_state.bandit.update(i, 1)
 
 arm = st.session_state.bandit.pull()
-st.write(f"Suggesting {fruits[arm]} for the next pick!")
+
+
+def update(arm, reward):
+    st.session_state.bandit.update(arm, reward)
+
+
+st.title("Fruit Bandit")
+st.image(images[arm])
+st.write(f"Do you like {fruits[arm]}?")
+
+yes, no = st.columns(2)
+yes.button(
+    "Yes", type="primary", use_container_width=True, on_click=update, args=[arm, True]
+)
+no.button(
+    "No", type="secondary", use_container_width=True, on_click=update, args=[arm, False]
+)
+
 for i in range(n_arms):
-    success = int(st.session_state.bandit.n_success[i])
-    failure = int(st.session_state.bandit.n_failure[i])
-    st.write(f"{fruits[i]}: {success}/{success+failure}")
+    bandit = st.session_state.bandit
+    success = int(bandit.n_success[i])
+    failure = int(bandit.n_failure[i])
+    st.write(f"{fruits[i]}: {success}/{failure} ({success+failure})")
